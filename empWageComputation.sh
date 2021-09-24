763e9df79c1b33a7cdd7eacd068edd01ef855e3b
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-24 19:14:04 +0530

    Refactor the code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 
-IS_FULL_TIME=2;
-IS_PART_TIME=1;
-MAX_HOUR_IN_MONTH=100;
-EMPL_RATE_PER_HOUR=20;
-NUM_OF_WORKING_DAYS=20;
+IS_FULL_TIME=1
+IS_PART_TIME=2
+EMPL_RATE_PER_HR=20
+MAX_HOURS_IN_MONTH=100
+MAX_WORKING_DAYS=20
 
-TOTAL_WORKING_HOURS=0;
-TOTAL_WORKING_DAYS=0;
+TOTAL_EMP_HRS=0
+TOTAL_WORKING_DAYS=0
 
-function getWorkingHours() {
-        case $1 in
-                $IS_FULL_TIME)
-                        WORK_HOURS=8
-                        ;;
-                $IS_PART_TIME)
-                        WORK_HOURS=4
-                        ;;
-                *)
-                        WORK_HOURS=0
-                        ;;
-        esac
+function getWorkingHrs() {
+	case $1 in
+		$IS_FULL_TIME)
+			EMPL_HOURS=8
+			;;
+		$IS_PART_TIME)
+			EMPL_HOURS=4
+			;;
+		*)
+			EMPL_HOURS=0
+			;;
+	esac
 }
-
-while [[ $TOTAL_WORKING_HOURS -lt $MAX_HOUR_IN_MONTH &&
-        $TOTAL_WORKING_DAYS -lt $NUM_OF_WORKING_DAYS ]]
+while [[ $TOTAL_EMP_HRS -lt $MAX_HOURS_IN_MONTH && $TOTAL_WORKING_DAYS -lt $MAX_WORKING_DAYS ]]
 do
         ((TOTAL_WORKING_DAYS++))
-        getWorkingHours $((RANDOM%3))
-        TOTAL_WORKING_HOURS=$(($TOTAL_WORKING_HOURS+$WORK_HOURS));
+        getWorkingHrs $((RANDOM%3))
+        TOTAL_EMP_HRS=$(($TOTAL_EMP_HRS+$EMPL_HOURS))
 done
-TOTAL_SALARY=$(($TOTAL_WORKING_DAYS*$EMPL_RATE_PER_HOUR));
+TOTAL_SALARY=$(($TOTAL_EMP_HRS * $EMPL_RATE_PER_HR))
+echo "Total Salary :- " $TOTAL_SALARY
+
