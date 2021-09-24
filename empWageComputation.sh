64d8bfa666107b172809f68a82fa1c95680c47be
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-24 18:44:31 +0530

    Switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,14 +2,18 @@
 echo "Welcome to Employee Wage Computation Program"
 
 IS_PART_TIME=1;
+IS_FULL_TIME=2;
 EMPL_RATE_PER_HOUR=20;
-EMPL_CHECK=$((RANDOM % 2))
+EMPL_CHECK=$((RANDOM % 3));
 case $EMPL_CHECK in
-        $IS_PART_TIME)
+       $IS_FULL_TIME)
            EMPL_HOURS=8
            ;;
-        *)
-           EMPL_HOURS=0
+       $IS_PART_TIME)
+           EMPL_HOURS=4
            ;;
+       *)
+            EMPL_HOURS=0
+            ;;
 esac
-SALARY_PART_TIME=$(($EMPL_HOURS * $EMPL_RATE_PER_HOUR))
+EMPL_SALARY=$(($EMPL_HOURS*$EMPL_RATE_PER_HOUR));
