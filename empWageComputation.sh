bfe715df464ccf33254976bcaacd3bcb82be8f3f
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-24 18:27:57 +0530

    Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 
-#!/bin/bash -x
-IS_PRESENT=0;
-RANDOM_CHECK=$((RANDOM % 2));
-if [ $IS_PRESENT -eq $RANDOM_CHECK ]
-then
-    echo "Employee is Present"
-else
-    echo "Emplyee is Absent"
-fi
+EMPL1=1;
+EMPL2=2;
+EMPL3=3
+WAGE_PER_HOUR=20;
+EMPL_CHECK=$((RANDOM % 4))
+case $EMPL_CHECK in
+        $EMPL1)
+            EMPL_HOURS=8
+            ;;
+        $EMPL2)
+            EMPL_HOURS=4
+            ;;
+        $EMPL3)
+            EMPL_HOURS=2
+            ;;
+        *)
+esac
+TOTAL_WAGE_IN_DAYS=$(($EMPL_HOURS * $WAGE_PER_HOUR))
