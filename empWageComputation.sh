23ae98dd7a3c2b065b1a22c8d53307e78aae645f
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-24 19:23:02 +0530

    Store the daily wage along with the total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,13 +4,8 @@ echo "Welcome to Employee Wage Computation Program"
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMPL_RATE_PER_HR=20
-MAX_HOURS_IN_MONTH=100
-MAX_WORKING_DAYS=20
-
-TOTAL_EMP_HRS=0
-TOTAL_WORKING_DAYS=0
-
-function getWorkingHrs() {
+WORKING_DAY=0
+function getWorkingHrs () {
 	case $1 in
 		$IS_FULL_TIME)
 			EMPL_HOURS=8
@@ -22,13 +17,17 @@ function getWorkingHrs() {
 			EMPL_HOURS=0
 			;;
 	esac
+	echo $EMPL_HOURS
 }
-while [[ $TOTAL_EMP_HRS -lt $MAX_HOURS_IN_MONTH && $TOTAL_WORKING_DAYS -lt $MAX_WORKING_DAYS ]]
-do
-        ((TOTAL_WORKING_DAYS++))
-        getWorkingHrs $((RANDOM%3))
-        TOTAL_EMP_HRS=$(($TOTAL_EMP_HRS+$EMPL_HOURS))
-done
-TOTAL_SALARY=$(($TOTAL_EMP_HRS * $EMPL_RATE_PER_HR))
-echo "Total Salary :- " $TOTAL_SALARY
+function getEmpWage() {
+	echo $(($1*$EMPL_RATE_PER_HR))
+}
 
+((WORKING_DAY++))
+EMPL_CHECK=$((RANDOM%3))
+EMPL_HOURS=$(getWorkingHrs $EMPL_CHECK)
+DAILY_WAGES[$WORKING_DAY]=$(($EMPL_RATE_PER_HR*$EMPL_HOURS))
+
+echo ${DAILY_WAGES[@]}
+TOTAL_SALARY="$(getEmpWage $EMPL_HOURS)"
+echo "Total Salary : "$TOTAL_SALARY
